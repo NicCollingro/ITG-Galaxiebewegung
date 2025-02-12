aaa7647b7a420987f9d389e96524f8ab16da5146
NicCollingro <nic@collingro.de>
2025-02-12 18:24:40 +0100

    .

---
+++ Code/AnimationStartValues.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file="/Users/niccollingro/Desktop/Universitaet des Saarlandes/WiSe 24:25/ITG/ITG-Galaxiebewegung/cmake-build-debug/Startwerte.txt"
+
+coordinateX = []
+coordinateY = []
+coordinateRadius = []
+coordinateDegree = []
+
+with open(file, "r") as f:
+    for line in f:
+        values = line.strip().split("\t")  # Zeile aufsplitten (Tab als Trennzeichen)
+        coordinateRadius.append(float(values[0]))
+        coordinateDegree.append(float(values[1]))
+
+coordinateX = coordinateRadius * np.sin(coordinateDegree)
+coordinateY = coordinateRadius * np.cos(coordinateDegree)
+
+fig, ax = plt.subplots(figsize=(40, 40), dpi=100)
+
+fig.patch.set_facecolor('black')
+ax.set_facecolor('black')
+
+ax.scatter(coordinateX, coordinateY, color='white', s=1)
+
+ax.set_xticks([])  # Entfernt x-Achsen-Ticks
+ax.set_yticks([])  # Entfernt y-Achsen-Ticks
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.set_xticklabels([])  # Entfernt x-Achsen-Beschriftungen
+ax.set_yticklabels([])
+
+plt.show()
--- Code/SimulationCreateStartValues.cpp
+++ Code/SimulationCreateStartValues.cpp
@@ -1,41 +1,51 @@
 #include <cstdio>
 #include <random>
 #include <cmath>
+#include <iostream>
+#include <fstream>
 
 struct Coordinate {
 public:
-    int mass;
-    int x;
-    int y;
-    float distanceFromCenter;
-    float velocityX;
-    float velocityY;
+    float mass;
+    float radius;
+    float degree;
+    float velocityRadius;
+    float velocity;
 };
 
 
 int main() {
+    int entries = 100000;
     float minMass = 0.08;
     float maxMass = 300.0;
     float gravConst = 6.67430e-11;
     float centerMass = 3.7e6;
     int radiusOfGalaxy = 100000;
-    Coordinate coordinates[100000];
+    Coordinate coordinates[entries];
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(-radiusOfGalaxy, radiusOfGalaxy);
-    std::uniform_real_distribution<float> distrib2(minMass, maxMass);
-    for (int i = 0; i < 100000; i++) {
-        coordinates[i].x = distrib(gen);
-        coordinates[i].y = distrib(gen);
-        coordinates[i].mass = distrib2(gen);
-        coordinates[i].distanceFromCenter = sqrt(pow(coordinates[i].x,2) + pow(coordinates[i].y,2));
-        if (coordinates[i].x == 0 && coordinates[i].y == 0) {
+    std::uniform_real_distribution<float> distrib1(0, radiusOfGalaxy);
+    std::uniform_real_distribution<float> distrib2(0, 2*M_PI);
+    std::uniform_real_distribution<float> distrib3(minMass, maxMass);
+    for (int i = 0; i < entries; i++) {
+        coordinates[i].radius = distrib1(gen);
+        coordinates[i].degree = distrib2(gen);
+        coordinates[i].mass = distrib3(gen);
+        if (coordinates[i].radius == 0){
             i--;
         }
-        float theta = std::atan2(coordinates[i].y, coordinates[i].x);
-        float velocity = std::sqrt((gravConst * centerMass) / coordinates[i].distanceFromCenter);
-        coordinates[i].velocityX = -velocity * std::sin(theta);
-        coordinates[i].velocityY = velocity * std::cos(theta);
+        float velocity = std::sqrt((gravConst * centerMass) / coordinates[i].radius);
+        coordinates[i].velocityRadius = 0;
+        coordinates[i].velocityY = velocity * std::cos(coordinates[i].degree);
     }
+
+    std::ofstream file("Startwerte.txt", std::ios::trunc);
+    if (!file){
+        std::cout << "Error creating file" << std::endl;
+    }
+    for (int i = 0; i < entries; i++) {
+        file << coordinates[i].radius << "\t" << coordinates[i].degree << "\t" << coordinates[i].velocityX << "\t" << coordinates[i].velocityY << "t" << coordinates[i].mass <<"\n";
+    }
+
     return 0;
 }
